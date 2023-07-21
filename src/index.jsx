c4e636630332f42e92de5d04a935cf7c1a1ca3a4
Jimmy Clairy <jimcl@hotmail.fr>
2023-07-21 15:42:39 +0200

    end useContext

--- src/index.jsx
+++ src/index.jsx
@@ -7,39 +7,29 @@ import Header from './layouts/Header'
 import Error from './components/Error/Error'
 import Results from './pages/Results/Results'
 import Freelances from './pages/Freelances/Freelances'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-    *{
-        box-sizing:border-box;
-    }
-
-    html {
-        font-size:62.5%
-    }
-
-    body {
-        font-size:1.6rem;
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-        width:90%;
-        max-width:120rem;
-        margin: auto;
-    }
-`
+import Footer from './layouts/Footer'
+import { ThemeProvider } from './utils/Context/ThemeProvider'
+import GlobalStyle from './utils/style/GlobalStyle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <Router>
-            <GlobalStyle />
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/survey/:questionNumber" element={<Survey />} />
-                <Route path="/results" element={<Results />} />
-                <Route path="/freelances" element={<Freelances />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
+            <ThemeProvider>
+                <GlobalStyle />
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/survey/:questionNumber"
+                        element={<Survey />}
+                    />
+                    <Route path="/results" element={<Results />} />
+                    <Route path="/freelances" element={<Freelances />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+                <Footer />
+            </ThemeProvider>
         </Router>
     </React.StrictMode>
 )
---
+++ src/utils/style/GlobalStyle.jsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../Context/ThemeProvider'
+import { createGlobalStyle } from 'styled-components'
+
+const StyledGlobalStyle = createGlobalStyle`
+*{
+    box-sizing:border-box;
+}
+
+html {
+    font-size:62.5%
+}
+
+body {
+    font-size:1.6rem;
+    font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    width:90%;
+    max-width:120rem;
+    margin: auto;
+    background-color: ${({ isDarkMode }) => (isDarkMode ? '#2F2E41' : 'white')};
+}
+`
+
+function GlobalStyle() {
+    const { theme } = useContext(ThemeContext)
+    return <StyledGlobalStyle isDarkMode={theme === 'dark'} />
+}
+
+export default GlobalStyle
